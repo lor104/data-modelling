e913deb5fcc0a8915ba7b18f92d6c4d51305ea31
lor104 <lauren.rosentzveig@gmail.com>
2017-04-18 14:39:47 -0400

    write model associations

---
+++ ex2/cookbook/db/migrate/20170418183236_create_join_table.rb
@@ -0,0 +1,8 @@
+class CreateJoinTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :ingridients_recipes do |t|
+      t.integer :ingredient_id
+      t.integer :recipe_id
+    end
+  end
+end
